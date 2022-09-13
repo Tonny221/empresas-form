378174ae936ede112b7192aca32579e7cfdb50f2
Tonny221 <anthonny14gabriell@gmail.com>
2022-09-13 13:48:35 -0400

    cleaning code

--- src/containers/InfoData.js
+++ src/containers/InfoData.js
@@ -1,7 +1,7 @@
 import { Paper, Typography, styled, Container, InputLabel, TextField, Grid } from '@mui/material'
 import React from 'react'
 
-const ContentPaper = styled(Paper)(({ }) => ({
+const ContentPaper = styled(Paper)(({ theme }) => ({
     marginTop: '2rem',
     padding: '1.5rem'
 }));
@@ -10,15 +10,15 @@ const InfoData = () => {
     return (
         <Container>
             <ContentPaper elevation={24}>
-                <Typography variant='h2' textAlign={'center'} fontSize={{xs: '2.25rem', sm: '4rem'}}>Dados da empresa</Typography>
-                <Grid container spacing={8} justifyContent='center' mt={{ sm: '0.25rem'}} p={{xs: 1, sm : 4}}>
+                <Typography variant='h2' textAlign={'center'} fontSize={{ xs: '2.25rem', sm: '4rem' }}>Dados da empresa</Typography>
+                <Grid container spacing={8} justifyContent='center' mt={{ sm: '0.25rem' }} p={{ xs: 1, sm: 4 }}>
                     <Grid item xs={12} sm={6}>
                         <InputLabel>Nome fantasia da empresa:</InputLabel>
                         <TextField variant='standard' fullWidth />
                     </Grid>
                     <Grid item xs={12} sm={6}>
                         <InputLabel>Cidade onde a empresa atua:</InputLabel>
-                        <TextField variant='standard' fullWidth />
+                        <TextField variant='standard' fullWidth placeholder='Adicionar endereço completo' />
                     </Grid>
                     <Grid item xs={12} sm={6}>
                         <InputLabel>Nome:</InputLabel>
--- src/ui/Header.js
+++ src/ui/Header.js
@@ -1,29 +1,29 @@
 import { Box, Container, styled, Typography } from '@mui/material'
 import React from 'react'
 
-const HeaderBox = styled(Box)(({}) => ({
+const HeaderBox = styled(Box)(({ theme }) => ({
     width: '100vw',
     backgroundColor: '#001f7d',
     color: 'white',
     padding: '1rem'
 }));
 
-const HeaderContainer = styled(Container)(({}) => ({
+const HeaderContainer = styled(Container)(({ theme }) => ({
     textAlign: 'center'
 }));
 
-const HeaderTitle = styled(Typography)(({}) => ({
+const HeaderTitle = styled(Typography)(({ theme }) => ({
     fontWeight: '200'
 }));
 
 const Header = () => {
-  return (
-    <HeaderBox>
-        <HeaderContainer>
-            <HeaderTitle variant='h3'>Empresas parceiras</HeaderTitle>
-        </HeaderContainer>
-    </HeaderBox>
-  )
+    return (
+        <HeaderBox>
+            <HeaderContainer>
+                <HeaderTitle variant='h3'>Empresas parceiras</HeaderTitle>
+            </HeaderContainer>
+        </HeaderBox>
+    )
 }
 
 export default Header
